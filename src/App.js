60a8ac2c106c378f25283de6e482ab2d267c6b28
Kevin Morehouse <40247525+kevinmorehouse@users.noreply.github.com>
2022-10-26 15:35:22 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set, onValue } from "firebase/database";
 
-function App() {
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+const App = () => {
+  const [text, setText] = useState("");
+
+  const messageRef = ref(database, "strings/");
+
+  const writeString = (str) => {
+    set(ref(database, "strings/"), {
+      message: str,
+    });
+  };
+
+  const handleChange = (e) => {
+    const text = e.target.value;
+    setText(text);
+    writeString(text);
+  };
+
+  useEffect(() => {
+    onValue(messageRef, (snapshot) => {
+      const data = snapshot.val();
+      setText(data.message);
+    });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <textarea value={text} onChange={handleChange}></textarea>
     </div>
   );
-}
+};
 
 export default App;
