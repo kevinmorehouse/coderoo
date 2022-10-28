6c393fd031eab97e2a558debf7665a39902c0920
Kevin Morehouse <40247525+kevinmorehouse@users.noreply.github.com>
2022-10-28 19:40:05 -0400

    Add room signup/access form to Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [formText, setFormText] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/room/${formText}`);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          id='room-signup'
+          placeholder='Enter a room name'
+          value={formText}
+          onChange={handleChange}
+          required
+        ></input>
+        <input type='submit' value='Submit' />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
