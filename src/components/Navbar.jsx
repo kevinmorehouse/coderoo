57e0d16d0d90336151099ddf45046744f01f1a15
Kevin Morehouse <40247525+kevinmorehouse@users.noreply.github.com>
2022-10-29 23:54:52 -0400

    Add homepage link and current room name to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,44 @@
 import React from "react";
-import { AppBar, Toolbar } from "@mui/material";
+import { AppBar, Toolbar, Typography, Link } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const location = useLocation();
+  const roomId =
+    location.pathname.length > 1
+      ? location.pathname.match(/[^(?:/room/)].*/g)[0]
+      : "";
+
   return (
     <>
-      <AppBar position='sticky' color='primary'>
-        <Toolbar>Coderoo</Toolbar>
+      <AppBar
+        position='absolute'
+        color='default'
+        elevation={0}
+        sx={{
+          position: "relative",
+          borderBottom: (t) => `1px solid ${t.palette.divider}`,
+        }}
+      >
+        <Toolbar>
+          <Link
+            variant='button'
+            href='/'
+            sx={{
+              my: 1,
+              mx: 1.5,
+              textDecoration: "none",
+              color: "black",
+              fontSize: "18px",
+            }}
+          >
+            Coderoo
+          </Link>
+          <Typography variant='button' color='primary'>
+            {roomId ? "Current Room: " + roomId : ""}
+          </Typography>
+        </Toolbar>
       </AppBar>
       <Outlet />
     </>
