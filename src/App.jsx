e6707f229c3846659660080b3ba7cc1f22215c29
Kevin Morehouse <40247525+kevinmorehouse@users.noreply.github.com>
2022-11-01 21:41:40 -0400

    Move 'create/join room' form to CreateOrJoinRoom

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Room from "./components/Room";
 import Navbar from "./components/Navbar";
+import CreateOrJoinRoom from "./components/CreateOrJoinRoom";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/join-room' element={<CreateOrJoinRoom />} />
         <Route path='/room/:roomId' element={<Room />} />
       </Routes>
     </>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,67 +1,7 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import CodeIcon from "@mui/icons-material/Code";
+import React from "react";
 
 const Home = () => {
-  const [formText, setFormText] = useState("");
-  const navigate = useNavigate();
-
-  const handleChange = (e) => {
-    const text = e.target.value.toLowerCase();
-    setFormText(text);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    navigate(`/room/${formText}`);
-  };
-
-  return (
-    <Container component='main' maxWidth='xs'>
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
-          <CodeIcon />
-        </Avatar>
-        <Typography component='h1' variant='h5'>
-          Create or Join a Room
-        </Typography>
-        <Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin='normal'
-            required
-            fullWidth
-            id='room'
-            label='Room Name'
-            name='room'
-            autoComplete='room'
-            autoFocus
-            onChange={handleChange}
-          />
-          <Button
-            type='submit'
-            fullWidth
-            variant='contained'
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Enter room
-          </Button>
-        </Box>
-      </Box>
-    </Container>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
