e23c35de3c7567c7ec70818a81d5e2240dfd15db
Kevin Morehouse <40247525+kevinmorehouse@users.noreply.github.com>
2022-11-12 17:01:02 -0500

    Update render test

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { themeOptions } from "./theme";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme(themeOptions);
 
 describe("App component", () => {
   test("it renders", () => {
     render(
       <BrowserRouter>
-        <App />
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
       </BrowserRouter>
     );
-    expect(screen.getByText("Click here to get started")).toBeInTheDocument();
+    expect(
+      screen.getByText("Click here to get started 😄")
+    ).toBeInTheDocument();
   });
 });
